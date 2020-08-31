f03aa6f307546e78423a1c9c859cc0b925c10493
Mohak <mohakchamp@gmail.com>
2020-08-31 20:35:54 +0530

    Just started

---
+++ 1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n,i,x;
+	int a[10];
+	
+	cout<<"Enter n: "<<endl;
+	cin>>n;
+
+	cout<<"Enter array: "<<endl;
+	for(int i = 0; i < n ; i++){
+		cin>>a[i];
+	}
+	
+	x=a[0];
+	
+	for(int i = 1; i < n ; i++){
+		x ^= a[i];
+	}
+	
+	cout<<"Odd occurence element: "<<endl;
+    cout<<x;
+    
+	return 0;
+}
---
+++ 2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+void reverse(int *a, int start,int end)
+{
+    while(start<end)
+    {
+    int temp;
+    temp=a[start];
+    a[start]=a[end];
+    a[end]=temp;
+    start++;
+    end--;
+    }
+}
+
+int main()
+{
+	int n,i,d;
+	int a[10];
+	
+	cout<<"Enter n: "<<endl;
+	cin>>n;
+
+    cout<<"Enter array: "<<endl;
+	for(i = 0; i < n ; i++){
+		cin>>a[i];
+	}
+	
+	cout<<"Enter d: "<<endl;
+	cin>>d;
+	
+    reverse(a,0,d-1);
+    reverse(a,d,n-1);
+    reverse(a,0,n-1);
+    
+    cout<<"Left Rotated array:"<<endl;
+    for(i = 0; i < n ; i++)
+    {
+		cout<<a[i]<<" ";
+	}
+	
+	return 0;
+}
